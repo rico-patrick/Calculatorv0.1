b178ec27b5e47ad5590449c6a3dbccabe50ab718
Rico <ricoedison@gmail.com>
2020-07-02 08:42:42 +0530

    Label added

--- Calc.py
+++ Calc.py
@@ -3,8 +3,11 @@ from tkinter import *
 window = Tk()
 window.geometry("400x500")
 window.title("Calculator")
-button = Button(text="ok", width=30, height=30)
-label = Label(window, text="")
+window.configure(bg="LIGHT GREY")
+label = Label(window, text="Helo", width=400, height=3, justify=RIGHT, fg="BLACK", bg="WHITE")
 label.pack()
+button = Button(text="7", height=2, width=100)
+button_frame.columncon
+button.grid(row=0, column=0)
 button.pack()
 window.mainloop()
