597ca871e4ca4ae4ce9b81fd02b15d87ffa2d47e
Rico <ricoedison@gmail.com>
2020-07-02 08:02:50 +0530

    Window

--- Calc.py
+++ Calc.py
@@ -1,8 +1,10 @@
 from tkinter import *
 
 window = Tk()
+window.geometry("400x500")
+window.title("Calculator")
 button = Button(text="ok", width=30, height=30)
-label=Label(window, text="welcome")
+label = Label(window, text="")
 label.pack()
 button.pack()
 window.mainloop()
